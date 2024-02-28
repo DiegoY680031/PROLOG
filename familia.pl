d218ec3d55c3ac5239b65e010cc8ee5f32018c04
Diego <alextintor680031@gmail.com>
2024-02-27 21:27:06 -0400

    Clase 27 de febrero

---
+++ familia.pl
@@ -0,0 +1,24 @@
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+
+esMujer(mariel).
+esMujer(cristal).
+esMujer(alejandra).
+
+esHijo(cristiano, juan).
+esHijo(cristiano, batista).
+esHijo(mariel, batista).
+esHijo(mariel, cristal).
+esHijo(batista, alejandra).
+esHijo(cristal, alejandra).
+esHijo(batista, adrian).
+esHijo(cristal, adrian).
+esHijo(batista, luciano).
+esHijo(cristal, luciano).
+esHijo(mario, alvaro).
+esHijo(alejandra, alvaro).
